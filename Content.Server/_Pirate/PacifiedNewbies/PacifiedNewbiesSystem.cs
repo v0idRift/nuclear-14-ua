d408ad0f41b571528a4a66ef300158cec8ef7f8c
v0id-rift <void-rift@users.noreply.github.com>
2024-10-24 11:45:19 +0300

    pacificatiom

---
+++ Content.Server/_Pirate/PacifiedNewbies/PacifiedNewbiesSystem.cs
@@ -0,0 +1,39 @@
+using Content.Server._Pirate.ForcePacify;
+using Content.Server.GameTicking;
+using Content.Server.Players.PlayTimeTracking;
+using Content.Shared.CombatMode.Pacification;
+using Robust.Server.Player;
+
+namespace Content.Server._Pirate.PacifiedNewbies;
+
+public sealed class PacifiedNewbiesSystem : EntitySystem
+{
+    [Dependency] private readonly PlayTimeTrackingManager _playTimeTracking = default!;
+    private static readonly PiratePacifyManager _piratePacifyManager = new();
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PlayerSpawnCompleteEvent>(OnPlayerSpawningEvent);
+        SubscribeLocalEvent<GhostRoleTakenPirate>(OnPlayerGhostRoleTaken);
+    }
+
+    private void OnPlayerGhostRoleTaken(GhostRoleTakenPirate ev)
+    {
+        var overall = _playTimeTracking.GetOverallPlaytime(ev.Player);
+        if (overall.TotalHours <= 1 || _piratePacifyManager.IsPacified(ev.Player.Name))
+        {
+            EnsureComp<PacifiedComponent>(ev.Mob);
+        }
+    }
+
+    private void OnPlayerSpawningEvent(PlayerSpawnCompleteEvent ev)
+    {
+        var overall = _playTimeTracking.GetOverallPlaytime(ev.Player);
+        if (overall.TotalHours <= 1 || _piratePacifyManager.IsPacified(ev.Player.Name))
+        {
+            EnsureComp<PacifiedComponent>(ev.Mob);
+        }
+    }
+}
